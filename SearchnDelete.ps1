f7cc20c9ea5c7805cb0f5ac21db8613a6d3ad4c4
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-01-09 12:01:45 -0800

    Create SearchnDelete.ps1
    Search and delete any filename from a computer

---
+++ SearchnDelete.ps1
@@ -0,0 +1,12 @@
+$filename = $arg[0]
+
+# Search for the file
+$file = Get-ChildItem -Path "C:\" -recurse -Filter $filename -ErrorAction  SilentlyContinue
+
+# If the file was found, delete it
+if ($file) {
+  Remove-Item $file.FullName
+  Write-Output "Deleted $filename"
+} else {
+  Write-Output "File not found"
+}
